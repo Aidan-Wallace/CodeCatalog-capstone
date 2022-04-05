debbe7e451e329993896f955eb7414ecf72a851f
John Fulton <john@techelevator.com>
2022-04-04 21:33:22 -0400

    initial commit

---
+++ dotnet/Capstone/DAO/Interfaces/IUserDao.cs
@@ -0,0 +1,12 @@
+﻿using Capstone.Models;
+using System.Collections.Generic;
+
+namespace Capstone.DAO.Interfaces
+{
+    public interface IUserDao
+    {
+        User GetUser(string username);
+        List<User> GetUsers();
+        User AddUser(string username, string password, string role);
+    }
+}
