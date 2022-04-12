94f60862371fe63413a857513f3ac531af746f63
Jack Updyke <jackupdyke6@gmail.com>
2022-04-12 12:11:30 -0400

    update

---
+++ dotnet/Capstone/DAO/Interfaces/ISearchQueryDAO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone.DAO.Interfaces
+{
+    public class ISearchQueryDAO
+    {
+        SearchQuery SearchQuery(string title);
+    }
+}
