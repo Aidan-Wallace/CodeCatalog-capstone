f2f313cb2bc842cac6ecc4a20a3b8cbeedb486a5
Aidan Wallace <aidanswallace@gmail.com>
2022-04-13 17:09:31 -0400

    Re-did some styling

--- vue/src/components/CatalogExample.vue
+++ vue/src/components/CatalogExample.vue
@@ -1,53 +1,70 @@
 <template>
-  <div class="example-container" :key="reRenderKey">
-    <!-- {{ currentExample.title }} -->
-    <!-- 
-          TITLE
+  <div class="example-container" v-bind:class="{ expanded: !isExpanded }">
+    <div class="example-title">
+      <h2>
+        {{ example.title }}
+      </h2>
+    </div>
 
-          CODE DISPLAY
+    <div class="example-code">
+      {{ example.codeSnippet }}
+    </div>
 
-          DESCRIPTION
+    <div class="example-description">
+      <p>
+        {{ example.codeDescription }}
+      </p>
+    </div>
 
-          REFS
+    <div class="example-date">Updated: {{ example.exampleDate }}</div>
 
-          KEYWORDS
-       -->
+    <div class="example-difficulty">
+      Experience level: {{ example.difficultyRank }}
+    </div>
+
+    <div class="example-category">code category: {{ example.category }}</div>
+
+    <div class="example-language">
+      {{ example.programmingLanguage }}
+    </div>
   </div>
 </template>
 
 <script>
-import CatalogService from "@/services/CatalogService";
-
 export default {
   name: "FullCatalog",
-  props: ["codeId"],
-  computed: {},
-  methods: {
-    setKey() {
-      this.reRenderKey++;
-    }
-  },
-  data() {
-    return {
-      currentExample: this.$store.state.currentExample,
-      isContentLoaded: false,
-      reRenderKey: 0,
-    };
-  },
-  created() {
-    CatalogService.getExample(1)
-      .then((res) => {
-        // this.$store.state.currentExample = res.data;
-
-        this.$store.commit("SET_CURRENT_EXAMPLE", res.data);
-        console.log(this.$store.state.currentExample);
-
-        // this.isContentLoaded = true;
-        // this.$forceUpdate();
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  },
+  /* *** PROP USES ***
+    - "example"    :: A object passed in with each code example.
+    - "isExpanded" :: A boolean to add proper styling to .example-container 
+        element to help reuse this component. If set to false(by default),
+        the component will return a closed and clickable element with just a 
+        few details rather than the whole code block.
+  */
+  props: ["example", "isExpanded"],
 };
 </script>
+
+<style scoped>
+.example-container {
+  width: 90vw;
+
+  border-radius: 14px;
+  box-shadow: 0 0 9px rgba(0, 0, 0, 0.4);
+  padding: 15px 0 15px 0;
+  margin-bottom: 25px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+
+.example-container.expanded {
+  display: none;
+}
+
+.example-code {
+  background-color: #eee;
+  padding: 5px;
+}
+</style>
---
+++ vue/src/components/SearchResults.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="search-results-container">
+      
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
