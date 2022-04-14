f05dd698bf545922d8bb6d0665b14dff5d985421
Patrick Arace <patrickarace@gmail.com>
2022-04-14 10:36:00 -0400

    download component

--- vue/src/components/Download.vue
+++ vue/src/components/Download.vue
@@ -6,10 +6,19 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
     methods: {
         downloadItem ({url, label}) {
-
+            axios.get(url, {responseType: 'blob'})
+            .then(response => {
+                const blob = new Blob([response.data], {type: ''})
+                const link = document.createElement('a')
+                link.href = URL.createObjectURL(blob)
+                link.download = label
+                link.click()
+                URL.revokeObjectURL(link.href)
+                }).catch(console.error)
         }
     }
 }
