6c2598d1826bbb900e591b9480c78d1b87c0fed6
Patrick Arace <patrickarace@gmail.com>
2022-04-14 09:28:57 -0400

    download component initialized

---
+++ vue/src/components/Download.vue
@@ -0,0 +1,16 @@
+<template>
+    <a 
+    :href="item.url"
+    v-text="item.label"
+    @click.prevent="downloadItem(item)" />
+</template>
+
+<script>
+export default {
+    methods: {
+        downloadItem ({url, label}) {
+
+        }
+    }
+}
+</script>
