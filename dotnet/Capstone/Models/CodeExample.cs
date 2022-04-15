a0b427ce4783b7501af5be63a6016230cada53e8
Misha Shatokhin <Shatokhingti@gmail.com>
2022-04-15 16:27:55 -0400

    completed status updates for admin

--- dotnet/Capstone/Models/CodeExample.cs
+++ dotnet/Capstone/Models/CodeExample.cs
@@ -5,21 +5,6 @@ namespace Capstone.Models
     public class CodeExample
     {
         public int codeId { get; set; }
-        public string title { get; set; }
-        public string programmingLanguage { get; set; }
-        public int submissionStatus { get; set; } = 0;
-        public string codeDescription { get; set; }
-        public string exampleDate { get; set; }
-        public string codeSnippet { get; set; }
-        public string difficultyRank { get; set; }
-        public string category { get; set; } //might have to make list/dict
-        public string keyword{get; set; }
-        public string attribution { get; set; }
-        public bool isPublic { get; set; } = false;
-    }
-    public class PendingExample
-    {
-        public int codeId { get; set; }
         public int submissionStatus { get; set; } = 0; //0=pending, 1=approved, 2=declined
         public string title { get; set; }
         public string programmingLanguage { get; set; }
@@ -30,6 +15,7 @@ namespace Capstone.Models
         public string category { get; set; } //might have to make list/dict
         public string attribution { get; set; }
         public bool isPublic { get; set; } = false;
+        public string keyword{get; set; }
     }
 
 }
