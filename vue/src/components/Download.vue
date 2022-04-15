c91248f55b2ca8741e568fe9c99832c145ff6f18
Patrick Arace <patrickarace@gmail.com>
2022-04-15 10:19:49 -0400

    dl and copy jawns

--- vue/src/components/Download.vue
+++ vue/src/components/Download.vue
@@ -1,27 +1,25 @@
 <template>
-    <a 
-    :href="download.url"
-    v-text="download.label"
-    @click.prevent="downloadExample({
-        url: `https://localhost:8080/download/${codeId}`,
-        label: 'Script download'
-    })" />
+<a
+    :href="item.url"
+    v-text="item.label"
+    @click.prevent="downloadItem(item)" />
 </template>
 
 <script>
-import axios from 'axios'
+import Axios from 'axios'
 export default {
   name: 'Download',
-  methods: {
-    async downloadItem({ url, label }) {
-      const response = await axios.get(url, { responseType: 'blob' });
-      const blob = new Blob([response.data], { type: 'application/json' });
-      const link = document.createElement('a');
-      link.href = URL.createObjectURL(blob);
-      link.download = label;
-      link.click();
-      URL.revokeObjectURL(link.href);
-    },
-  },
-};
+methods: {
+  downloadItem ({ url, label }) {
+    Axios.get(url, { responseType: 'blob' })
+      .then(response => {
+        const blob = new Blob([response.data], { type: 'application/json' })
+        const link = document.createElement('a')
+        link.href = URL.createObjectURL(blob)
+        link.download = label
+        link.click()
+        URL.revokeObjectURL(link.href)
+      }).catch(console.error)
+    }
+}};
 </script>
