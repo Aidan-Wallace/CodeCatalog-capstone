fa857df59c5fdbaa591db69dd46fd571005a547b
Aidan Wallace <aidanswallace@gmail.com>
2022-04-18 13:45:51 -0400

    Muchos updateos en la fronto-endo

---
+++ vue/src/components/FullScreenExample.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    {{ example.title }}
+    {{ example.programmingLanguage }}
+    {{ example.codeDescription }}
+    <div v-for="category in getCategories" :key="category">
+      {{ category }}
+    </div>
+    <div class="code-snippet">
+      {{ example.codeSnippet }}
+    </div>
+    <div v-for="attr in getAttributions" :key="attr">
+      {{ attr }}
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "FullScreenExample",
+  props: ["example"],
+  computed: {
+    getCategories() {
+      return this.example.category.split(" ");
+    },
+    getAttributions() {
+      return this.example.attribution.split(" ");
+    },
+  },
+  created() {
+    // DOES NO`T WORKf
+    if (typeof this.example != "object") {
+      this.$router.push({ path: "/" });
+    }
+  },
+};
+</script>
+
+<style>
+</style>
