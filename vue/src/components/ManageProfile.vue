6efae4572be0a8949770f41b40b7f6e5d619d9e6
Jack Updyke <jackupdyke6@gmail.com>
2022-04-20 13:57:31 -0400

    update

--- vue/src/components/ManageProfile.vue
+++ vue/src/components/ManageProfile.vue
@@ -1,18 +1,34 @@
 <template>
   <div>
     <h1>Manage Profile</h1>
+    <h1>Edit My Code</h1>
+    <div v-show="$store.state.user.role == 'admin'">
+      Manage Pending
+      <div v-for="example in pendingExamples" :key="example.codeId">
+        <catalog-example :example="example" />
+        <button v-on:click.prevent="updateExampleStatus(example.codeId, 1)">
+          Approve
+        </button>
+        <button v-on:click.prevent="updateExampleStatus(example.codeId, 2)">
+          Reject
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import ProfileService from "@/services/ProfileService";
+import CatalogExample from "./CatalogExample.vue";
 
 export default {
+  components: { CatalogExample },
   name: "ManageProfile",
   data() {
     return {
-      userId: this.$store.state.currentUser,
+      userId: this.$store.state.user.userId,
       examples: {},
+      pendingExamples: {},
     };
   },
 
@@ -30,9 +46,44 @@ export default {
           console.log(err);
         });
     },
+    getExamplesByStatus() {
+      ProfileService.getPending()
+        .then((response) => {
+          if (200 <= response.status && response.status < 300) {
+            console.log(response.status);
+
+            this.pendingExamples = response.data;
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    updateExampleStatus(codeId, status) {
+      
+      this.pendingExamples.foreach((example) => {
+        if (example.codeId == codeId) {
+          example.status=status;
+          ProfileService.updateExampleStatus(codeId, example)
+            .then((response) => {
+              if (200 <= response.status && response.status < 300) {
+                console.log(response.status);
+
+                this.pendingExamples = response.data;
+              }
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        }
+      })
+      
+    },
   },
   created() {
     this.getExamplesByUserId();
+    this.getExamplesByStatus();
+    this.updateExampleStatus();
   },
 };
 </script>
